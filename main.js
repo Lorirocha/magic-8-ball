19625d7815a48bf6f1f50b40457b93f119eb375b
lori rocha <lori.eileen05@gmail.com>
2022-12-12 21:07:44 -0800

    added answers for 8 ball

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   
     var magic8Ball = {};
-    magic8Ball.listofanswers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
+    magic8Ball.listofanswers = ["Umm maybe.", "Yeah right", "Without a doubt.", "Yes, definitely.", "You may rely on it.", "Shake me again", "Most likely.", "For sure.", "Yes.", "Signs point to yes.", "Don't know don't care.", "Ask again later.", "Better not tell you now.", "I don't think so.", "Think again.", "Don't count on it.", "In your dreams", "My heart says no.", "Anyways, Next.", "Battery Low."];
    
     magic8Ball.getAnswer = function(question)
     {
@@ -12,7 +12,7 @@ $(document).ready(function(){
       $("#8ball").effect( "shake" );
       $("#answer").text( answer );
       $("#answer").fadeIn(3000);
-      $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+      $("#8ball").attr("src", "https://us.123rf.com/450wm/tassel78/tassel781802/tassel78180200006/tassel78180200006.jpg?ver=6");
   
       console.log(question);
       console.log(answer);
@@ -22,10 +22,11 @@ $(document).ready(function(){
     var onClick = function()
     {
       $("#answer").hide();
-      $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
+      $("#8ball").attr("src", "https://us.123rf.com/450wm/tassel78/tassel781802/tassel78180200006/tassel78180200006.jpg?ver=6");
       var question = prompt("What do you want to know?");
       magic8Ball.getAnswer(question);
     };
     
     $("#questionButton").click( onClick );
   });
+  
